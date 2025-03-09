1923f058d77ffe7718951260560c9c4253e80ddc
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-09 20:46:57 +0500

    -eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,6 @@ export default [
       '@typescript-eslint/no-extraneous-class': 'error',
       '@typescript-eslint/no-invalid-void-type': 'error',
       '@typescript-eslint/no-misused-new': 'error',
-      '@typescript-eslint/no-namespace': 'error',
       '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
       '@typescript-eslint/no-non-null-assertion': 'error',
