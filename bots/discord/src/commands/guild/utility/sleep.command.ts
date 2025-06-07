47142d0f2521a4258fd526324e49e04dce1f2bae
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-06-08 02:31:35 +0500

    -up sleep > formatting ru word

--- bots/discord/src/commands/guild/utility/sleep.command.ts
+++ bots/discord/src/commands/guild/utility/sleep.command.ts
@@ -1,8 +1,12 @@
 import { CommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
 import { Types } from "@voidy/types";
 
+import Formatter from "f-formatter";
+
 import Sleep from "src/utility/service/sleep.service";
 
+const formatter = new Formatter();
+const format = (seconds: number) => formatter.RuWords(seconds, ["секунду", "секунды", "секунд"]);
 const TIMEOUT = 10;
 
 export default new Types.Discord.Command({
@@ -11,7 +15,7 @@ export default new Types.Discord.Command({
     interaction.reply({content: `Завершении программы через ${TIMEOUT} секунд.`, flags: MessageFlags.Ephemeral})
     
     new Sleep(TIMEOUT).execute((delay) => {
-      interaction.editReply({content:  `Завершении программы через ${delay} секунд.`});
+      interaction.editReply({content:  `Завершении программы через ${delay} ${format(delay)}.`});
     }).then(() => {
       interaction.editReply({content: "Программа завершена."});
     });
